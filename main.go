fa89f7f0ab66c0b6fddecf0b461e4b68f3d4f626
isaacwilkinsonlongden <isaacwilkinsonlongden@gmail.com>
2026-05-05 19:40:46 +0100

    added index.html and root handler

--- main.go
+++ main.go
@@ -6,15 +6,17 @@ import (
 )
 
 func main() {
+	const filepathroot = "."
 	const port = "8080"
 
 	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir(filepathroot)))
 
 	srv := &http.Server{
 		Addr:    ":" + port,
 		Handler: mux,
 	}
 
-	log.Printf("Serving on port: %s\n", port)
+	log.Printf("Serving files from %s on port: %s\n", filepathroot, port)
 	log.Fatal(srv.ListenAndServe())
 }
